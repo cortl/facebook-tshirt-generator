f929fee1bcb1de97c671ca713653ed6a03d648d2
Cortlan Bainbridge <bainbric@uni.edu>
2019-01-12 17:51:26 -0600

    adding firebase, changing favicon, and adding sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
 module.exports = {
-    plugins:  [     
-        {       
-          resolve: `gatsby-plugin-typography`,       
-          options: {         
-            pathToConfigModule: `src/utils/typography.js`,       
-        },     
-      },   
-    ],
+  siteMetadata: {
+    title: 'Facebook T-Shirt Generator',
+    siteUrl: 'tshirts.cortlan.co',
+    description: 'A T-Shirt generator for oddly specific audiences'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
 }
